3b034e4f3b2619c6ea1443a20b99c10bcdb09faf
Guilherme Rocha <79716932+guilherme-or@users.noreply.github.com>
2023-09-16 23:16:17 -0300

    Alteracoes nos modelos

--- src/apps/portal/admin.py
+++ src/apps/portal/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Usuario
+from .models import Usuario, Cor, Carro, Personalizacao, Solicitacao
 
 admin.site.register(Usuario)
+admin.site.register(Cor)
+admin.site.register(Carro)
+admin.site.register(Personalizacao)
+admin.site.register(Solicitacao)
--- src/apps/portal/models.py
+++ src/apps/portal/models.py
@@ -2,13 +2,72 @@ from django.db import models
 
 
 class Usuario(models.Model):
-    db_table = "usuarios"
-    id_usuario = models.IntegerField(primary_key=True, autoincrement=True)
     nome = models.CharField(max_length=128)
     sobrenome = models.CharField(max_length=128)
     admin = models.BooleanField(default=False)
     senha = models.CharField(max_length=128)
     data_nascimento = models.DateField("Data de nascimento")
     email = models.EmailField(max_length=128, unique=True)
-    celular = models.CharField(max_length=20)
-    cep = models.CharField(max_length=8)
+    celular = models.CharField(max_length=19, null=True)  # +55 (11) 91234-5678
+    cep = models.CharField(max_length=8)  # 12345678
+
+    class Meta:
+        db_table = "usuarios"
+        verbose_name_plural = "Usuários"
+
+    def __str__(self):
+        return self.nome
+
+
+class Cor(models.Model):
+    nome = models.CharField(max_length=32)
+    codigo = models.CharField(max_length=9)  # #RRGGBB00
+
+    class Meta:
+        db_table = "cores"
+        verbose_name_plural = "Cores"
+
+    def __str__(self):
+        return self.nome
+
+
+class Carro(models.Model):
+    modelo = models.CharField(max_length=64)
+    ano = models.PositiveIntegerField()  # 1999
+    cores = models.ManyToManyField(Cor)
+
+    class Meta:
+        db_table = "carros"
+        verbose_name_plural = "Carros"
+
+    def __str__(self):
+        return self.modelo
+
+
+class Personalizacao(models.Model):
+    nome = models.CharField(max_length=64)
+    descricao = models.TextField()
+    carros = models.ManyToManyField(Carro, related_name="carros")
+
+    class Meta:
+        db_table = "personalizacoes"
+        verbose_name_plural = "Personalizações"
+
+    def __str__(self):
+        return self.nome
+
+
+class Solicitacao(models.Model):
+    descricao = models.TextField()
+    data_criacao = models.DateTimeField("Data de criação")
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    personalizacoes = models.ManyToManyField(
+        Personalizacao, related_name="personalizacoes"
+    )
+
+    class Meta:
+        db_table = "solicitacoes"
+        verbose_name_plural = "Solicitações"
+
+    def __str__(self):
+        return self.data_criacao
