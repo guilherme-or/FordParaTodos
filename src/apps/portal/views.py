58e458d25fbaba3ab37018f2cd5165331faaebaf
Guilherme Rocha <79716932+guilherme-or@users.noreply.github.com>
2023-09-18 20:00:32 -0300

    Criação da Nav Bar e Inserção dos Logos

--- src/apps/portal/views.py
+++ src/apps/portal/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'portal/index.html')
+    return render(request, "portal/index.html")
+
+
 def catalogo(request):
-    return render(request, 'portal/catalogo.html')
+    return render(request, "portal/catalogo.html")
