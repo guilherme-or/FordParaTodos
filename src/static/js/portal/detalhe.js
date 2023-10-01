165d15736ea277714b8e6ed108aad91e4b87dcc1
Guilherme Rocha <79716932+guilherme-or@users.noreply.github.com>
2023-09-30 22:39:41 -0300

    shopping cart completo, menos formatacao

--- src/static/js/portal/detalhe.js
+++ src/static/js/portal/detalhe.js
@@ -1,3 +1,5 @@
+$('#preco_carro').mask('000.000.000.000.000,0', { reverse: true });
+
 document.querySelectorAll('.cor__input_radio').forEach(input => {
   input.addEventListener('change', () => {
     const labels = document.querySelectorAll('.cor__input_radio__label')
@@ -17,19 +19,59 @@ const firstInput = document.querySelector('.cor__input_radio');
 firstInput.checked = true;
 firstInput.dispatchEvent(new Event('change'));
 
-function manageShoppingCart(personalizacao) {
+function handleShoppingCart(personalizacao) {
+  function createShoppingCartItem(personalizacao) {
+    const cartItem = document.createElement('div');
+    cartItem.classList.add('d-flex', 'justify-content-between', 'align-items-center', 'mt-3', 'p-2', 'rounded');
+    cartItem.setAttribute('data-nome', personalizacao.id);
 
+    cartItem.innerHTML = `
+        <div class="d-flex flex-row">
+            <img src="/static/img/customizacoes/${personalizacao.id}.png"
+                class="rounded mx-2 my-2" alt="Imagem de ${personalizacao.id}" style="height: 50px; width: auto;">
+            <span class="fw-bold d-block mx-3">${personalizacao.id}</span>
+        </div>
+        <div class="d-flex flex-row align-items-center">
+            <span class="d-block ml-3 fw-bold">R$ ${personalizacao.dataset.preco}</span>
+        </div>
+    `;
+
+    return cartItem;
+  }
+
+  const listElement = document.getElementById('shopping-cart-items');
+  const existingCartItem = listElement.querySelector(`[data-nome="${personalizacao.id}"]`);
+  const subtotal = document.getElementById('subtotal');
+
+  if (existingCartItem) {
+    existingCartItem.remove();
+    subtotal.innerHTML = parseInt(subtotal.innerHTML) - parseInt(personalizacao.dataset.preco);
+  } else {
+    listElement.append(createShoppingCartItem(personalizacao));
+    subtotal.innerHTML = parseInt(subtotal.innerHTML) + parseInt(personalizacao.dataset.preco);
+  }
+
+  let itemsCount = listElement.childElementCount;
+  document.getElementById('shopping-cart-item-count-text').innerHTML =
+    itemsCount === 0 ?
+      'Você ainda não selecionou nenhum item' :
+      itemsCount === 1 ? 'Você tem 1 item selecionado' : `Você tem ${itemsCount} itens selecionados`;
+  document.getElementById('shopping-cart-item-count').innerHTML = itemsCount;
+  document.getElementById('total').innerHTML = parseInt(document.getElementById('preco_carro').innerHTML) + parseInt(subtotal.innerHTML);
+
+  // NÃO ESTÁ FORMATANDO ...
+  $('#subtotal').mask('000.000.000.000.000,0', { reverse: true });
+  $('#total').mask('000.000.000.000.000,0', { reverse: true });
 }
 
 document.querySelectorAll('.personalizacoes')
-  .forEach(personalizacao => personalizacao.addEventListener('change', e => {
-    const element = e.target;
+  .forEach(personalizacao => {
+    personalizacao.addEventListener('change', e => {
+      const elemento = e.target;
 
-    if (e.target.checked) {
-      document.getElementById(`checked-${element.id}`).classList.remove('d-none');
-      document.getElementById(`unchecked-${element.id}`).classList.add('d-none');
-    } else {
-      document.getElementById(`unchecked-${element.id}`).classList.remove('d-none');
-      document.getElementById(`checked-${element.id}`).classList.add('d-none');
-    }
-  }));
+      document.getElementById(`checked-${elemento.id}`).classList.toggle('d-none', !elemento.checked);
+      document.getElementById(`unchecked-${elemento.id}`).classList.toggle('d-none', elemento.checked);
+
+      handleShoppingCart(elemento);
+    });
+  });
